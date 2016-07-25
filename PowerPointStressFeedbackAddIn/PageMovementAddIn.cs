db39ea18969334204774c8713da87dac8ea2277f
Patrick Desjardins <mrdesjardins@gmail.com>
2016-07-25 12:25:27 -0700

    Power point Add-in - This project is an Add-In to PowerPoint that will run during a   presentation. - The goal is to send to the server the information about time and slide   number. We will be able to corrolate the session id from Microsoft Band   and Microsoft PowerPoint.

---
+++ PowerPointStressFeedbackAddIn/PageMovementAddIn.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+
+namespace PowerPointStressFeedbackAddIn
+{
+    public partial class PageMovementAddIn
+    {
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            this.Application.SlideShowBegin += OnSlideShowBegin;
+            this.Application.SlideShowEnd += OnSlideShowEnd;
+            this.Application.SlideShowOnNext += OnSlideNext;
+            this.Application.SlideShowOnPrevious += OnSlidePrevious;
+        }
+
+        private void OnSlidePrevious(PowerPoint.SlideShowWindow wn)
+        {
+            SendData(wn.View.Slide.SlideNumber);
+        }
+
+        private void OnSlideNext(PowerPoint.SlideShowWindow wn)
+        {
+            SendData(wn.View.Slide.SlideNumber);
+        }
+
+
+        private void OnSlideShowEnd(PowerPoint.Presentation pres)
+        {
+            SendData(-1);
+        }
+
+
+
+        private void OnSlideShowBegin(PowerPoint.SlideShowWindow wn)
+        {
+            SendData(wn.View.Slide.SlideNumber);
+        }
+
+
+        private void SendData(int slideNumber)
+        {
+            System.Diagnostics.Debug.WriteLine(DateTime.Now + ":" + slideNumber);
+        }
+
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
