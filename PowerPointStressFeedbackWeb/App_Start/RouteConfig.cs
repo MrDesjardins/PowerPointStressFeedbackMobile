673082371be851baff542bd9a34f56b4ea4d82f2
Patrick Desjardins <mrdesjardins@gmail.com>
2016-07-25 22:13:25 -0700

    - Clean up old Identity classes and configuration - Make date passed by parameter

--- PowerPointStressFeedbackWeb/App_Start/RouteConfig.cs
+++ PowerPointStressFeedbackWeb/App_Start/RouteConfig.cs
@@ -12,11 +12,11 @@ namespace PowerPointStressFeedbackWeb
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapMvcAttributeRoutes();
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{sessionId}",
+                defaults: new { controller = "StressFeedback", action = "Index", sessionId = UrlParameter.Optional }
             );
         }
     }
--- PowerPointStressFeedbackWeb/Controllers/StressFeedbackController.cs
+++ PowerPointStressFeedbackWeb/Controllers/StressFeedbackController.cs
@@ -14,19 +14,27 @@ namespace PowerPointStressFeedbackWeb.Controllers
         private static Dictionary<string, List<PowerPointData>> powerpointData = new Dictionary<string, List<PowerPointData>>();
         private static Dictionary<string, List<BandData>> bandData = new Dictionary<string, List<BandData>>();
 
+        [HttpGet]
+        public ActionResult Index(string sessionId)
+        {
+            if (sessionId != null)
+            {
+                var data = this.GetMergedData(sessionId);
+                return View(data);
+            }
+            return View();
+        }
 
         [HttpPost]
-        public ActionResult PowerPointData(PowerPointData item)
+        [Route("StressFeedback/PowerPointData/{sessionId}/{dateTime}/{slideIndex}")]
+        public ActionResult PowerPointData(string sessionId, string dateTime, int slideIndex)
         {
-            if (item == null)
+            var dateTime1 = tranformStringToDate(dateTime);
+            if (!powerpointData.ContainsKey(sessionId) || powerpointData[sessionId] == null)
             {
-                return this.HttpNotFound();
+                powerpointData[sessionId] = new List<Controllers.PowerPointData>();
             }
-            if (!powerpointData.ContainsKey(item.SessionId) || powerpointData[item.SessionId] == null)
-            {
-                powerpointData[item.SessionId] = new List<Controllers.PowerPointData>();
-            }
-            powerpointData[item.SessionId].Add(item);
+            powerpointData[sessionId].Add(new PowerPointData {SessionId = sessionId, DateTime= dateTime1, SlideIndex = slideIndex});
 
             return Json(new { status = "ok" });
         }
@@ -43,17 +51,14 @@ namespace PowerPointStressFeedbackWeb.Controllers
         }
 
         [HttpPost]
-        public ActionResult BandData(BandData item)
+        public ActionResult BandData(string sessionId, string dateTime, int hearbeat, double temperature)
         {
-            if (item == null)
+            var dateTime1 = tranformStringToDate(dateTime);
+            if (!bandData.ContainsKey(sessionId) || bandData[sessionId] == null)
             {
-                return this.HttpNotFound();
+                bandData[sessionId] = new List<Controllers.BandData>();
             }
-            if (!bandData.ContainsKey(item.SessionId) || bandData[item.SessionId] == null)
-            {
-                bandData[item.SessionId] = new List<Controllers.BandData>();
-            }
-            bandData[item.SessionId].Add(item);
+            bandData[sessionId].Add(new BandData { SessionId = sessionId, DateTime = dateTime1, HeartBeat = hearbeat, Temperature = temperature});
 
             return Json(new {status="ok"});
         }
@@ -68,6 +73,58 @@ namespace PowerPointStressFeedbackWeb.Controllers
 
             var item = bandData[sessionId];
             return Json(item);
+        }
+
+        public MergedData GetMergedData(string sessionId)
+        {
+            if (!StressFeedbackController.powerpointData.ContainsKey(sessionId) ||
+                !StressFeedbackController.bandData.ContainsKey(sessionId))
+            {
+                return null;
+            }
+            var powerPointData = StressFeedbackController.powerpointData[sessionId];
+            var bandData = StressFeedbackController.bandData[sessionId];
+            var mergedData = new MergedData();
+            mergedData.SessionId = sessionId;
+            mergedData.Data = new List<MergedDataItem>();
+            var indexBandData = 0;
+            for (int powerpointIndex = 0; powerpointIndex < powerPointData.Count; powerpointIndex++)
+            {
+                var pointData = powerPointData[powerpointIndex];
+                if (indexBandData >= bandData.Count)
+                {
+                    break;
+                }
+                //For each slide, we collect all the Band data to get Min, Average, Max
+                var item = new MergedDataItem();
+                item.SlideIndex = pointData.SlideIndex;
+                item.Time = pointData.DateTime;
+                var count = 0;
+                var sum = 0;
+                while (indexBandData < bandData.Count && bandData[indexBandData].DateTime<pointData.DateTime)
+                {
+                    count++;
+                    var dd = bandData[indexBandData];
+                    item.HeartBeatMinimum = dd.HeartBeat < item.HeartBeatMinimum ? dd.HeartBeat : item.HeartBeatMinimum;
+                    item.HeartBeatMaximum = dd.HeartBeat > item.HeartBeatMaximum ? dd.HeartBeat : item.HeartBeatMaximum;
+                    sum += dd.HeartBeat;
+                }
+                item.HeartBeatAverage = sum/count;
+                mergedData.Data.Add(item);
+            }
+
+            return mergedData;
+        }
+
+        /// <summary>
+        /// Transform 2016D07D25_23T33T55 => 2016-07-25 23:33:55 
+        /// </summary>
+        /// <param name="dateInString"></param>
+        /// <returns></returns>
+        private DateTime tranformStringToDate(string dateInString)
+        {
+            var d= dateInString.Replace("D", "-").Replace("T", ":").Replace("_", " ");
+            return DateTime.Parse(d, null, System.Globalization.DateTimeStyles.RoundtripKind);
         }
     }
 
@@ -86,5 +143,33 @@ namespace PowerPointStressFeedbackWeb.Controllers
     public class BandData : CommontData
     {
         public int HeartBeat { get; set; }
+        public double Temperature { get; set; }
+    }
+
+    public class MergedData
+    {
+        public string SessionId { get; set; }
+
+        public List<MergedDataItem> Data{get;set;}
+    }
+
+    public class MergedDataItem
+    {
+        public DateTime Time { get; set; }
+        public int SlideIndex { get; set; }
+
+        public int HeartBeatMinimum { get; set; }
+        public int HeartBeatMaximum { get; set; }
+        public int HeartBeatAverage { get; set; }
+
+        public double Temperature { get; set; }
+
+        public MergedDataItem()
+        {
+            this.HeartBeatMinimum = 100;
+            this.HeartBeatAverage = 0;
+            this.HeartBeatMaximum = 0;
+            this.Temperature = 0;
+        }
     }
 }
